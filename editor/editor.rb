e8a1e4aaa8c65edf4b2dbbfe3b9d92048931aa50
Farijo <vallarteddy@yahoo.fr>
2016-02-19 20:39:08 +0100

    coloration lors du survol des cases de l'éditeur

--- editor/editor.rb
+++ editor/editor.rb
@@ -21,10 +21,17 @@ class GameWindow < Gosu::Window
 				draw_line(i*50,j*50+50,Color::GRAY,i*50,j*50,Color::GRAY)
 			end
 		end
+		
+		x = (mouse_x/50).to_i
+		y = (mouse_y/50).to_i
+		
+		draw_rect(x*50, y*50+1, 49, 49, Color::RED)
 	end
 
 	def button_down(id)
-		
+		if id == Gosu::KbEscape then
+			close
+		end
 	end
 	
 	def needs_cursor?
